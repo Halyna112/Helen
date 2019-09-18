6a1a5c0a1fc7476d26a0803350405de8591dbf0b
Halyna112 <55506288+Halyna112@users.noreply.github.com>
2019-09-18 20:18:37 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+$(function(){
+    $('nav a').each(function(index, element){
+        // event.preventDefault();
+        $(element).click(function(){
+            const id=$(this).attr('href');
+            const top = $(id).offset().top;
+            $('html').animate({
+                scrollTop:top
+            },1500)
+        })
+    })
+})
